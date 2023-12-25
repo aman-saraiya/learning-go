e0d4aa2348370c5b58fa3d489b4e7db0350bd5af
aman-saraiya <amansaraiya937@gmail.com>
2023-12-26 02:42:19 +0530

    added basic details to the first program

--- hello_world.go
+++ hello_world.go
@@ -1,7 +1,11 @@
 package main
 
+// inbuilt package in Go
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	fmt.Print("Hello World") // prints to standard output
 }
+
+// We could use backticks (`) and double quotes for strings but not single
+// quotes in Go.
