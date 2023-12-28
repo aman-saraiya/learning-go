e9af6b25382234b9f29e21af1a63599a3a5afcd6
aman-saraiya <amansaraiya937@gmail.com>
2023-12-28 23:07:01 +0530

    updated the profit calculator to use functions

--- profit_calculator/main.go
+++ profit_calculator/main.go
@@ -4,16 +4,25 @@ import "fmt"
 
 func main() {
 	var revenue, expenses, taxRate float64
-	fmt.Println("Profit Calculator")
-	fmt.Print("Enter your revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Enter your tax rate: ")
-	fmt.Scan(&taxRate)
-	fmt.Print("Enter your expenses: ")
-	fmt.Scan(&expenses)
 
+	revenue = getUserInput("Enter your revenue: ")
+	taxRate = getUserInput("Enter your tax rate: ")
+	expenses = getUserInput("Enter your expenses: ")
+
+	calculateFinancials(revenue, expenses, taxRate)
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64) {
 	earningsBeforeTax := revenue - expenses
 	fmt.Println("Your Earnings Before Tax (EBT): ", earningsBeforeTax)
 	earningsAfterTax := revenue*(1-taxRate/100) - expenses
 	fmt.Print("Your Earnings After Tax (Profit): ", earningsAfterTax)
+	return earningsBeforeTax, earningsAfterTax
+}
+
+func getUserInput(prompt string) float64 {
+	fmt.Print(prompt)
+	var input float64
+	fmt.Scan(&input)
+	return input
 }
