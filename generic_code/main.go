1babbbc76219f00715ec007fe33949baff99b68d
aman-saraiya <amansaraiya937@gmail.com>
2023-12-30 16:27:13 +0530

    added details on working with generics

--- generic_code/main.go
+++ generic_code/main.go
@@ -1,12 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/aman-saraiya/learning-go/interfaces/note"
+)
 
 func main() {
+	myNote, _ := note.New("Pay Electricity Bill", "Due Date: 30/01/2024")
 	//working with type switches
 	workingWithTypeSwitches(12)
 	workingWithTypeSwitches(9.55)
 	workingWithTypeSwitches(myNote)
+
+	addApproachOne(2, 3)
+	addApproachOne(6.5, 1.2)
+	addApproachOne(myNote, myNote) // does nothing
+
+	output1 := addUsingGenerics(2, 10)
+	fmt.Println(output1)
+	output2 := addUsingGenerics(2.4, 1.4)
+	fmt.Println(output2)
 }
 
 // Function that can take any kind of value int, any struct, anything
@@ -43,3 +57,37 @@ func workingWithTypeSwitches(input interface{}) {
 		fmt.Println("Entered input is an integer", typedValue)
 	}
 }
+
+// Writing generic functions
+// we want to write an add function for example with works with all int, float
+// and string. If we use interface{} that will allow any types and not all types
+// support '+' operation and hence we'll get an error
+
+// approach 1
+func addApproachOne(a, b interface{}) interface{} {
+	aInt, aOk := a.(int)
+	bInt, bOk := a.(int)
+	if aOk && bOk {
+		fmt.Println("Sum of a and b:", aInt+bInt)
+		return aInt + bInt
+	}
+
+	aFloat, aOk := a.(float64)
+	bFloat, bOk := b.(float64)
+	if aOk && bOk {
+		fmt.Println("Sum of a and b:", aFloat+bFloat)
+		return aFloat + bFloat
+	}
+
+	return nil
+}
+
+// approach 2: Using generics
+// T here is a type placeholder, we could use any name
+// T <concrete types allowed in place of this placeholder>
+func addUsingGenerics[T int | string | float64](a, b T) T {
+	return a + b
+}
+
+// sample usage of multiple generic types
+func usingMultipleGenericTyoes[T any, K any](x T, y K) T { return x }
