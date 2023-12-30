c13fcac267c5b1c09aa739fe4674d7e9bc4d2acb
aman-saraiya <amansaraiya937@gmail.com>
2023-12-30 15:24:21 +0530

    Using interface to reduce code duplication

--- main.go
+++ main.go
@@ -18,12 +18,18 @@ func main() {
 		return
 	}
 	myNote.Display()
-	err = myNote.Save()
-	if err != nil {
-		fmt.Println("Saving the Note failed.")
-	} else {
-		fmt.Println("Note saved successfully to file.")
-	}
+	// duplicate code that could be removed by using interfaces
+	/*
+		err = myNote.Save()
+		if err != nil {
+			fmt.Println("Saving the Note failed.")
+		} else {
+			fmt.Println("Note saved successfully to file.")
+		}
+	*/
+
+	// duplication removed using interfaces
+	saveData(myNote)
 
 	content = getTodoData()
 	myTodo, err := todo.New(content)
@@ -32,13 +38,44 @@ func main() {
 		return
 	}
 	myTodo.Display()
-	err = myTodo.Save()
+	// duplicate code that could be removed by using interfaces
+	/*
+		err = myTodo.Save()
+		if err != nil {
+			fmt.Println("Saving the Todo failed.")
+		} else {
+			fmt.Println("Todo saved successfully to file.")
+		}
+	*/
+
+	//duplication removed using interfaces
+	saveData(myTodo)
+
+	//NOTE: We're able to pass myTodo and myNote in place where a
+	// saver type is required because saver is an interface and it
+	// just enforces that a valid value for it must implement
+	// Save() method. And both Todo and Note have Save method.
+}
+
+// we cannot specify note.Note or todo.Todo as input param type if we want
+// to use this function with both types. We need interfaces.
+func saveData(dataStruct saver) {
+	err := dataStruct.Save()
 	if err != nil {
-		fmt.Println("Saving the Todo failed.")
+		fmt.Println("Saving the data struct failed.")
 	} else {
-		fmt.Println("Todo saved successfully to file.")
+		fmt.Println("Data struct saved successfully to file.")
 	}
 }
+
+// creating interface
+type saver interface {
+	Save() error
+}
+
+// side note, there's a convention in Go that if we define an interface with
+// only one method, than our interface name is method_name + "er", it's not a
+// must do but a common convention
 
 func getUserInput(prompt string) string {
 	fmt.Print(prompt)
